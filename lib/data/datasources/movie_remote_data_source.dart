49450f83e3c0d4a79459bcb61bf9f067bc1096f6
KarlJan Reginaldo <karljan.reginaldo@cvsu.edu.ph>
2021-05-28 15:52:01 +0800

    fix: refactoring data source and repository

--- lib/data/datasources/movie_remote_data_source.dart
+++ lib/data/datasources/movie_remote_data_source.dart
@@ -8,13 +8,12 @@ import '../models/movie_model.dart';
 import 'package:http/http.dart' as http;
 
 abstract class MovieRemoteDataSource {
-  ///Link https://api.themoviedb.org/3/movie/$id?api_key=API_KEY
   Future<MovieDetailModel> getSpecificMovie({@required int id});
 
-  ///Link:https://api.themoviedb.org/3/search/movie?api_key=API_KEY&query=$query
   Future<List<MovieModel>> searchMovies({@required String query});
 
   Future<List<MovieModel>> getInitialPopularMovies();
+
   Future<List<MovieModel>> chooseGenre({@required int genreID});
 }
 
@@ -37,52 +36,20 @@ class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
   }
 
   @override
-  Future<List<MovieModel>> searchMovies({String query}) async {
-    final _response = await _client.get(
-      'https://api.themoviedb.org/3/search/movie?api_key=$API_KEY&query=$query',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    );
-    final Map<String, dynamic> decoded = json.decode(_response.body);
-
-    final List<dynamic> movies = decoded['results'];
-
-    List<MovieModel> movieList = [];
-
-    movies.forEach((json) {
-      movieList.add(MovieModel.fromJson(json));
-    });
-
-    return _response.statusCode == 200 ? movieList : throw ServerException();
-  }
+  Future<List<MovieModel>> searchMovies({String query}) async => _getMovieList(
+      'https://api.themoviedb.org/3/search/movie?api_key=$API_KEY&query=$query');
 
   @override
-  Future<List<MovieModel>> getInitialPopularMovies() async {
-    //The page I set is randomized with maximum of 500.
-    final _response = await _client.get(
-      'https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY&language=en-US&page=${Random().nextInt(500)}',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    );
-    final Map<String, dynamic> decoded = json.decode(_response.body);
-
-    final List<dynamic> movies = decoded['results'];
-
-    List<MovieModel> movieList = [];
-
-    movies.forEach((json) {
-      movieList.add(MovieModel.fromJson(json));
-    });
-
-    return _response.statusCode == 200 ? movieList : throw ServerException();
-  }
+  Future<List<MovieModel>> getInitialPopularMovies() => _getMovieList(
+      'https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY&language=en-US&page=${Random().nextInt(500)}');
 
   @override
-  Future<List<MovieModel>> chooseGenre({int genreID}) async {
+  Future<List<MovieModel>> chooseGenre({int genreID}) async => _getMovieList(
+      'https://api.themoviedb.org/3/discover/movie?api_key=$API_KEY&with_genres=$genreID');
+
+  Future<List<MovieModel>> _getMovieList(String url) async {
     final _response = await _client.get(
-      'https://api.themoviedb.org/3/discover/movie?api_key=$API_KEY&with_genres=$genreID',
+      url,
       headers: {
         'Content-Type': 'application/json',
       },
--- lib/data/repositories/movie_repository_impl.dart
+++ lib/data/repositories/movie_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:themovie/data/models/movie_model.dart';
+
 import '../../core/error/exception.dart';
 import '../../core/error/failure.dart';
 import 'package:dartz/dartz.dart';
@@ -8,27 +10,29 @@ import '../../domain/entities/movie_detail.dart';
 import '../../domain/entities/movie.dart';
 import '../../domain/repositories/movie_repository.dart';
 
+typedef Future<List<MovieModel>> _MovieList();
+
 class MovieRepositoryImpl implements MovieRepository {
   final MovieRemoteDataSource _remote;
   final MovieLocalDataSource _local;
   MovieRepositoryImpl(this._remote, this._local);
 
   @override
-  Future<Either<Failure, MovieDetail>> getSpecificMovie({int id}) async {
-    try {
-      final _movie = await _remote.getSpecificMovie(id: id);
-      return Right(_movie);
-    } on ServerException {
-      return Left(
-        ServerFailure(),
-      );
-    }
-  }
+  Future<Either<Failure, List<Movie>>> searchMovies({String query}) async =>
+      _getMovies(() => _remote.searchMovies(query: query));
 
   @override
-  Future<Either<Failure, List<Movie>>> searchMovies({String query}) async {
+  Future<Either<Failure, List<Movie>>> getInitialPopularMovies() async =>
+      _getMovies(() => _remote.getInitialPopularMovies());
+
+  @override
+  Future<Either<Failure, List<Movie>>> chooseGenre({int genreID}) async =>
+      _getMovies(() => _remote.chooseGenre(genreID: genreID));
+
+  Future<Either<Failure, List<Movie>>> _getMovies(
+      _MovieList voidCallBack) async {
     try {
-      final _movies = await _remote.searchMovies(query: query);
+      final _movies = await voidCallBack();
       return Right(_movies);
     } on ServerException {
       return Left(
@@ -38,22 +42,10 @@ class MovieRepositoryImpl implements MovieRepository {
   }
 
   @override
-  Future<Either<Failure, List<Movie>>> getInitialPopularMovies() async {
+  Future<Either<Failure, MovieDetail>> getSpecificMovie({int id}) async {
     try {
-      final _popularMovies = await _remote.getInitialPopularMovies();
-      return Right(_popularMovies);
-    } on ServerException {
-      return Left(
-        ServerFailure(),
-      );
-    }
-  }
-
-  @override
-  Future<Either<Failure, List<Movie>>> chooseGenre({int genreID}) async {
-    try {
-      final _chosenGenreMovies = await _remote.chooseGenre(genreID: genreID);
-      return Right(_chosenGenreMovies);
+      final _movie = await _remote.getSpecificMovie(id: id);
+      return Right(_movie);
     } on ServerException {
       return Left(
         ServerFailure(),
